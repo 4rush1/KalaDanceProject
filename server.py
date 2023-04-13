a1714cbcc78e08c1664668b18b8201807ed9aaa7
Arushi Bhatnagar-Stewart <arushibhatnagar-stewart@ARUSHI-MacBook-Pro-3.local>
2023-04-13 18:52:49 +1200

    Added a dictionary so that my forms can be temporarily filled in, which saves time

--- server.py
+++ server.py
@@ -16,7 +16,14 @@ def NewsEvents():
 
 @app.route('/enrol', methods=["GET", "POST"])
 def enrol():
-    return render_template("enrol.html")
+    temp_form_data={
+        "firstname" : "Janet",
+        "surname" : "Jackson",
+        "email" : "jj@gmail.com",
+        "phone" : "04568765434",
+        "aboutme" : "I am a good dancer",
+    }
+    return render_template("enrol.html", **temp_form_data)
 
 if __name__ == "__main__":
     app.run(debug=True)
