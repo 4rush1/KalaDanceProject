d8458393dd2e7b0a8a5efe3a007c91b8af484654
Arushi Bhatnagar-Stewart <arushibhatnagar-stewart@ARUSHI-MacBook-Pro-3.local>
2023-04-13 19:16:30 +1200

    imported 'request' at the top of my server page, as well as incorperated the 'GET' and 'POST' commands using an 'if, elif' statement. I've also made a 'confirm' html page

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -16,14 +16,19 @@ def NewsEvents():
 
 @app.route('/enrol', methods=["GET", "POST"])
 def enrol():
-    temp_form_data={
-        "firstname" : "Janet",
-        "surname" : "Jackson",
-        "email" : "jj@gmail.com",
-        "phone" : "04568765434",
-        "aboutme" : "I am a good dancer",
-    }
-    return render_template("enrol.html", **temp_form_data)
+    if request.method == "POST":
+        f = request.form
+        print(f)
+        return "Posted"
+    elif request.method == "GET":
+        temp_form_data={
+            "firstname" : "Janet",
+            "surname" : "Jackson",
+            "email" : "jj@gmail.com",
+            "phone" : "04568765434",
+            "aboutme" : "I am a good dancer",
+        }
+        return render_template("enrol.html", **temp_form_data)
 
 if __name__ == "__main__":
     app.run(debug=True)
