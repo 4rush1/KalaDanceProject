8401ad08056e6e953d432865e0d046334500021e
Arushi Bhatnagar-Stewart <arushibhatnagar-stewart@ARUSHI-MacBook-Pro-3.local>
2023-05-15 23:06:07 +1200

    Started making my first table

--- data/create_db.sql
+++ data/create_db.sql
@@ -2,3 +2,11 @@
 
 drop table if exists news;
 drop table if exists member;
+
+create table member(
+    member_id integer primary key autoincrement not null
+    name text not null,
+    email text not null unique,
+    age_group text not null,
+    about_me text null
+)
