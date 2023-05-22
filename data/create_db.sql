2c8fcf2d1ce212e097e3607a8c5e221c5c00e478
Arushi Bhatnagar-Stewart <arushibhatnagar-stewart@ARUSHI-MacBook-Pro-3.local>
2023-05-22 15:00:27 +1200

    created my database file

--- data/create_db.sql
+++ data/create_db.sql
@@ -4,19 +4,19 @@ drop table if exists news;
 drop table if exists member; 
 
 create table member(
-    member_id integer primary key autoincrement not null
+    member_id integer primary key autoincrement,
     name text not null,
-    email text not null unique,
+    email text  not null unique,
     age_group text not null,
-    about_me text null
-)
+    about_me text
+);
 
 create table news(
-    news_id integer primary key autoincrement not null
+    news_id integer primary key autoincrement,
     title text not null unique,
     subtitle text not null unique,
     content text not null unique,
-    news_date date not not null,
+    news_date date not null,
     member_id integer not null,
     foreign key(member_id) references member(member_id)
-)
+);
