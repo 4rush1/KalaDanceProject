973bc0a9e5b853ffbb51eb9cc2c190f27606eb4b
Arushi Bhatnagar-Stewart <arushibhatnagar-stewart@ARUSHI-MacBook-Pro-3.local>
2023-06-01 11:29:35 +1200

    fixed some silly mistakes I had, such as having a function in a function, this caused my database to not work, I also added some text to my initial info page

--- server.py
+++ server.py
@@ -1,18 +1,33 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
+from db_function import run_search_query_tuples
+from datetime import datetime
 
 app = Flask(__name__)
+db_path = 'data/dance_db.sqlite'
+
 
 @app.route('/')
 def index():
     return render_template("index.html")
 
+
 @app.route('/initialinfo')
 def BeginnerInfo():
     return render_template("initial_info.html")
 
+
 @app.route('/news')
 def news():
-    return render_template("news.html")
+    # query for the news page
+    sql = """ select news.news_id, news.title, news.subtitle, news.content, news.news_date, member.firstname 
+    from news 
+    join member on news.member_id = member.member_id
+    order by news.news_date desc;
+    """
+    result = run_search_query_tuples(sql, (), db_path, True)
+    print(result)
+    return render_template("news.html", news=result)
+
 
 @app.route('/enrol', methods=["GET", "POST"])
 def enrol():
@@ -30,6 +45,7 @@ def enrol():
             "aboutme" : "I am a good dancer",
         }
         return render_template("enrol.html", **temp_form_data)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
--- testing.py
+++ testing.py
@@ -1,11 +1,12 @@
 from db_function import run_search_query_tuples
 
+# query test #
+# query for the news page #
 def get_news(db_path):  # stands for path #
     sql = """ select news.title, news.subtitle, news.content, member.firstname 
     from news 
     join member on news.member_id = member.member_id;
     """
-
     result = run_search_query_tuples(sql, (), db_path, True)
     for row in result:
         for k in row.keys():
