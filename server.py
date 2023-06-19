e1facceeb2aac2a22816a0e51906e7672a141013
Arushi Bhatnagar-Stewart <arushibhatnagar-stewart@ARUSHI-MacBook-Pro-3.local>
2023-06-20 00:34:01 +1200

    Finished the update task in the news page, now the user can update the news and this is added to the databse. previosuly, I also worked on the delete task, where the user can now delete news items. I still need to add a confirmation page for this task

--- server.py
+++ server.py
@@ -44,25 +44,62 @@ def news_cud():
         if k not in data.keys():
             message = "do not know what to do with create, read, update on news (key not present)"
             return render_template("error.html", message=message)
-    if request.method == "GET":
 
+# GET GET GET GET GET GET
+    if request.method == "GET":
         if data['task'] == 'delete':
+            # first query, the ? could be any number, it's an unknown variable
             sql = "delete from news where news_id = ?"
+            # unknown variable declared in tuple
             values_tuple = (data['id'],)
+            # tuple is passed into the result
             result = run_commit_query(sql, values_tuple, db_path)
             print("delete")
             print(result)
+            # redirects us back to the news page after deleting
             return redirect(url_for('news'))
         elif data['task'] == 'update':
-            return "<h1>I want to update</h1>"
+            sql = """ select title, subtitle, content from news where news_id=? """
+            values_tuple = (data['id'],)
+            result = run_search_query_tuples(sql, values_tuple, db_path, True)
+            # comes back in a list, just wants the first value
+            result = result[0]
+            return render_template("news_cud.html",
+                                   **result,
+                                   id=data['id'],
+                                   task=data['task'])
         elif data['task'] == 'add':
-            return render_template("news_cud.html", id=0, task=data['task'])
+            temp = {'title':'test title', 'subtitle':'test subtitle', 'content':'test content'}
+            return render_template("news_cud.html",
+                                   id=0,
+                                   task=data['task'],
+                                   **temp)
         else:
             message = "Unrecognised task coming from news page"
             return render_template("error.html", message=message)
-
+# POST POST POST POST POST
     elif request.method == "POST":
-        return "<h1>I want to update</h1>"
+        # collected form info
+        f = request.form
+        print(f)
+        if data['task'] == 'add':
+            # add the new news entry to the database
+            # could be different title, subtitle and content each time --> ???
+            # the datetime now is a sqlite command --> gets time off server and add it in SQLite format
+            # member is fixed at 2 for now, but will change later depending on who is logged in
+            sql = """insert into news(title, subtitle, content, newsdate, member_id)
+                        values(?,?,?, datetime('now', 'localtime'),2)"""
+            # tuple values
+            values_tuple = (f['title'], f['title'], f['content'])
+            result = run_commit_query(sql, values_tuple, db_path)
+            return redirect(url_for('news'))
+        elif data['task'] == 'update':
+            sql = """update news set title=?, subtitle=?, content=?, newsdate=datetime('now', 'localtime') where news_id=?"""
+            # tuple values
+            values_tuple = (f['title'], f['subtitle'], f['content'], data['id'])
+            result = run_commit_query(sql, values_tuple, db_path)
+            # collect the data from the form and update the database to the sent id
+            return redirect(url_for('news'))
     return render_template("news_cud.html")
 
 @app.route('/enrol', methods=["GET", "POST"])
